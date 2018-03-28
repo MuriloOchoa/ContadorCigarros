1377a4a7318e090c9e5f15111866eb7465cb90b3
MuriloOchoa <31893442+MuriloOchoa@users.noreply.github.com>
2018-03-28 05:56:21 -0300

    Adição do cálculo do valor gasto
    Adição do cálculo sobre o valor total gasto. Adequação de código ao padrão correto.

--- cigarro.rb
+++ cigarro.rb
@@ -1,24 +1,48 @@
-puts "Bem vindo, digite o seu nome:"
-nome = gets
+class Cigarro
+  #
+  #attr_accessor :nome, :cigdia, :totcarteira, :vlrcarteira, :anos
+  #
+  #def initialize(nome, cigdia, totcarteira, vlrcarteira, anos)
+  #  @nome, @cigdia, @totcarteira, @vlrcarteira, @anos = nome, cigdia, totcarteira, vlrcarteira, anos,
+  #end
 
-puts
-puts
 
-puts "Digite quantos cigarros voce fuma por dia: "
-cigdia = gets
+  puts "Bem vindo, digite o seu nome:"
+  @nome = gets
 
-puts
+  puts
+  puts
 
-puts "Digite quantos cigarros vem em cada carteira que voce compra: "
-totcarteira = gets
+  puts "Digite quantos cigarros voce fuma por dia: "
+  @cigdia = gets.chomp.to_i
 
-puts
+  puts
 
-puts "Digite quanto custa cada carteira que voce compra: "
-vlrcarteira = gets
+  puts "Digite quantos cigarros vem em cada carteira que voce compra: "
+  @totcarteira = gets.chomp.to_i
 
-puts 
+  puts
 
-puts "Digite a quantos anos voce fuma: "
-anos = gets
+  puts "Digite quanto custa cada carteira que voce compra: "
+  @vlrcarteira = gets.chomp.to_f
+
+  puts
+
+  puts "Digite a quantos anos voce fuma: "
+  @anos = gets.chomp.to_i
+
+  puts @vlrfinal = ((@cigdia * (@anos * 365)) / @totcarteira) * @vlrcarteira
+
+  puts "o valor total gasto com cigarros foi de: " +@vlrfinal.to_s
+
+  puts "digite algo para sair"
+  @sair = gets.chomp.to_i
+end
+
+
+
+
+
+
+
 
