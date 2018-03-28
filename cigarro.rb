eb4aa57540a971f1b25a03bf677c71e12adcefb6
MuriloOchoa <31893442+MuriloOchoa@users.noreply.github.com>
2018-03-28 03:54:09 -0300

    obtencaoDados
    Questionário inicial, perguntas básicas para obter os dados necessários para realizar conta do valor gasto até então com cigarros

---
+++ cigarro.rb
@@ -0,0 +1,24 @@
+puts "Bem vindo, digite o seu nome:"
+nome = gets
+
+puts
+puts
+
+puts "Digite quantos cigarros voce fuma por dia: "
+cigdia = gets
+
+puts
+
+puts "Digite quantos cigarros vem em cada carteira que voce compra: "
+totcarteira = gets
+
+puts
+
+puts "Digite quanto custa cada carteira que voce compra: "
+vlrcarteira = gets
+
+puts 
+
+puts "Digite a quantos anos voce fuma: "
+anos = gets
+
